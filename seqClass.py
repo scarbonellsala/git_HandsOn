19812074ba85ab0393ddcce66318e15c11e60b02
Silvia <silvia.carbonell@crg.eu>
2023-02-19 17:58:17 +0100

    motif added

--- seqClass.py
+++ seqClass.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -27,13 +28,8 @@ else:
 
 if args.motif:
     args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence 
-"{args.seq}"... ', end = '')
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
         print("FOUND")
     else:
         print("NOT FOUND")
-
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
-
-
